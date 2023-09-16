39028f49336782a3767baa3de77cf1f3543658ae
Peyton Meredith <peymeredith@gmail.com>
2023-09-15 19:39:21 -0500

    tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    addTaskToList(e.target.newTaskDescription.value)
+    form.reset()
+  }
+   )
+  
+   function addTaskToList(task){
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+    btn.addEventListener('click', e => {
+      e.target.parentNode.remove()
+    })
+    li.textContent = `${task}   `
+    btn.textContent = 'X'
+    li.append(btn)
+    document.getElementById('tasks').append(li)
+    console.log(li.textContent)
+   }
+
+
+
+
+
+
+
+  })
